c8c8e0d0a714895cb1722954339b9c858b233ba1
Jhonr17 <jhonmrodriguez17@gmail.com>
2026-04-08 15:25:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// EFECTO HEADER PEQUEÑO AL HACER SCROLL
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
+
+if(header){
 
 if(window.scrollY > 80){
 header.classList.add("header-small");
@@ -8,13 +12,24 @@ header.classList.add("header-small");
 header.classList.remove("header-small");
 }
 
+}
+
 });
+
+
+// MENU HAMBURGUESA
+
+document.addEventListener("DOMContentLoaded", function(){
 
 const toggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 
-if(toggle){
-toggle.addEventListener("click", () => {
+if(toggle && menu){
+
+toggle.addEventListener("click", function(){
 menu.classList.toggle("active");
 });
+
 }
+
+});
